cf137c325ed7a7a5067b2cb2fdef95440be21e90
Dudsztor <dudsztor.dudsztor@gmail.com>
2025-12-13 18:54:00 +0100

    nearest enemy arrow

--- cookie-jam-2025/Scripts/slashpivot.gd
+++ cookie-jam-2025/Scripts/slashpivot.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 @onready var player: CharacterBody2D = $".."
 @onready var atkcd: Timer = $atkcd
 var can_atk = true
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,9 +14,11 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact"):
 		if can_atk:
 			look_at(get_global_mouse_position())
-			rotation -= deg_to_rad(90)
+			rotation -= deg_to_rad(-90)
 			can_atk = false
 			atkcd.start()
+			anim_sprite.frame = 0
+			anim_sprite.play("default")
 
 
 func _on_atkcd_timeout() -> void:
