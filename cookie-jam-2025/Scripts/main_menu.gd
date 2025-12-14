78652f22327485dc18f18e7b59a41c214e918706
Dudsztor <dudsztor.dudsztor@gmail.com>
2025-12-14 15:43:36 +0100

    menu

--- cookie-jam-2025/Scripts/main_menu.gd
+++ cookie-jam-2025/Scripts/main_menu.gd
@@ -4,9 +4,9 @@ const GAME_SCENE_PATH = "res://Scenes/intro.tscn"
 const TUTORIAL_SCENE_PATH = "res://Scenes/Levels/level_tutorial.tscn"
 
 func _ready():
-	$VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
-	$VBoxContainer/TutorialButton.pressed.connect(_on_tutorial_pressed)
-	$VBoxContainer/QuitButton.pressed.connect(_on_quit_pressed)
+	$PlayButton.pressed.connect(_on_play_pressed)
+	$TutorialButton.pressed.connect(_on_tutorial_pressed)
+	$QuitButton.pressed.connect(_on_quit_pressed)
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file(GAME_SCENE_PATH)
